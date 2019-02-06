62a4a0b5226a5bdf703b523d4311d153c581d36f
Lukasz Lawicki <lukasz.lawicki@wp.pl>
2019-02-06 22:36:21 +0100

    Get page source and display in popup

--- popup.js
+++ popup.js
@@ -1,7 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-        chrome.tabs.getSelected(null, function (tab) {
-        var tablink = tab.url;
-        console.log(tablink);
-        document.getElementById("test").innerHTML = tablink;
+chrome.runtime.onMessage.addListener(function(request, sender) {
+    if (request.action == "getSource") {
+      message.innerText = request.source;
+    }
+  });
+  
+  function onWindowLoad() {
+  
+    var message = document.querySelector('#message');
+  
+    chrome.tabs.executeScript(null, {
+      file: "getPagesSource.js"
+    }, function() {
+      // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+      if (chrome.runtime.lastError) {
+        message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+      }
     });
-});
+  
+  }
+  
+  window.onload = onWindowLoad;
