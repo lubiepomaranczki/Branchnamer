ddd3efe44741ab97768ac97dfa40774d5fe1b9b6
Lukasz Lawicki <lukasz.lawicki@wp.pl>
2019-02-20 20:27:37 +0100

    Get elements from string

--- popup.js
+++ popup.js
@@ -1,9 +1,26 @@
 chrome.runtime.onMessage.addListener(function(request, sender) {
     if (request.action == "getSource") {
-      message.innerText = request.source;
+      message.innerText = getTitle(request.source);
     }
   });
   
+function getTitle(htmlCode){
+  doc = new DOMParser().parseFromString(htmlCode, "text/html");
+  if(typeof(doc)==='undefined') {
+    return "parser failed";
+  }
+  test="<p id=\"demo\" onclick=\"myFunction()\">Click me to change my HTML content (innerHTML).</p>"
+  doc = new DOMParser().parseFromString(test, "text/html")
+
+return "Penis "+ doc.getElementById("demo").innerHTML;
+}
+  // test = "<div class="editable-div" contentEditable="true">Hey</div>"
+  // doc = new DOMParser().parseFromString(htmlCode, "text/html");
+
+  // $('.editable-div').html()
+  // return doc.getElementsByClassName("ui-dialog-title").innerHTML;
+  // return doc.firstChild.innerText;
+
   function onWindowLoad() {
   
     var message = document.querySelector('#message');
