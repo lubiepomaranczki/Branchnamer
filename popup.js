d1db5f24d07bbf6b804216cb2fff4a34810974c3
Lukasz Lawicki <lukasz.lawicki@wp.pl>
2019-02-25 21:28:44 +0100

    Get page name

--- popup.js
+++ popup.js
@@ -1,31 +1,57 @@
-chrome.runtime.onMessage.addListener(function(request, sender) {
-    if (request.action == "getSource") {
-      message.innerText = getTitle(request.source);
-    }
-  });
-  
-function getTitle(htmlCode){
+chrome.runtime.onMessage.addListener(function (request, sender) {
+  if (request.action == "getSource") {
+    message.innerText = getTitle(request.source);
+  }
+});
+
+function getTitle(htmlCode) {
+// return htmlCode;
+
   doc = new DOMParser().parseFromString(htmlCode, "text/html");
-  if(typeof(doc)==='undefined') {
+  if (typeof (doc) === 'undefined') {
     return "parser failed";
   }
 
-  return "Penis "+ doc.getElementById("PopupContentContainer").innerHTML;
+  return doc.title;
+
+  // popupContentText = doc.getElementsByClassName("ui-dialog-title");
+
+  // var result = ""
+  // for(var i=0;i < popupContentText.length; i++){
+  //   result = result + (popupContentText[i].textContent || popupContentText[i].innerText) + "\n";
+  // }
+  // return result;
+  // return popupContentText.length;
+
+  // popupContentText = doc.getElementById("PopupContentContainer");
+  // if (typeof (doc) === 'undefinded') {
+  //   return "Hey, is it VSO/TFS/Azure Devops site you are on?"
+  // }
+  // popContent = new DOMParser().parseFromString(popupContentText, "text/html");
+  
+  // elements =  popContent.getElementsByTagName("BODY");
+  // // elements =  popContent.getElementsByClassName("rich-content-tooltip");
+  // if(typeof(elements)==='undefined')  {
+  //   return "Can't find";
+  // }
+
+  // // return htmlCode;
+  // return elements[0].innerHTML;  
 }
 
-  function onWindowLoad() {
-  
-    var message = document.querySelector('#message');
-  
-    chrome.tabs.executeScript(null, {
-      file: "getPagesSource.js"
-    }, function() {
-      // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-      if (chrome.runtime.lastError) {
-        message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-      }
-    });
-  
-  }
-  
-  window.onload = onWindowLoad;
+function onWindowLoad() {
+
+  var message = document.querySelector('#message');
+
+  chrome.tabs.executeScript(null, {
+    file: "getPagesSource.js"
+  }, function () {
+    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+    if (chrome.runtime.lastError) {
+      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+    }
+  });
+
+}
+
+window.onload = onWindowLoad;
