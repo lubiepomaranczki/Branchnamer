8c860580754ac52fee38187f05cf711923625eed
Lukasz Lawicki <lukasz.lawicki@wp.pl>
2019-02-27 19:14:40 +0100

    Added branch name generator on taskboard

--- popup.js
+++ popup.js
@@ -2,14 +2,18 @@ var siteUrl;
 
 document.addEventListener('DOMContentLoaded', () => {
   chrome.tabs.getSelected(null, function (tab) {
-  siteUrl = tab.url;
-});
+    siteUrl = tab.url;
+  });
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender) {
   if (request.action == "getSource") {
-    if(siteUrl.includes("workitems"))
-    message.innerText = getBranchnameForWorkitems(request.source);
+    if (siteUrl.includes("workitems")) {
+      message.innerText = getBranchnameForWorkitems(request.source);
+    }
+    else if (siteUrl.includes("taskboard")) {
+      message.innerText = getBranchnameForTaskboard(request.source);
+    }
   }
 });
 
@@ -25,10 +29,35 @@ function getBranchnameForWorkitems(htmlCode) {
   var itemNumber = docTitle.replace(/^\D+/g, ''); // replace all leading non-digits with nothing
   var withoutBoards = itemNumber.split(" - Boards")[0]
 
-  var str = withoutBoards.replace(/\s+/g, '-');
-  var result = str.replace(/:/g, '').toLowerCase();
+  return removeSpacesAndReplaceDashes(withoutBoards);
+}
 
-  return result;
+function getBranchnameForTaskboard(htmlCode) {
+
+  doc = new DOMParser().parseFromString(htmlCode, "text/html");
+  if (typeof (doc) === 'undefined') {
+    return "parser failed";
+  }
+
+  var result = "";
+  var dialogTitles = doc.getElementsByClassName("ui-dialog-title");
+
+  for (var i = 0; i < dialogTitles.length - 1; i++) {
+    if (hasNumber(dialogTitles[i].innerHTML)) {
+      var text = dialogTitles[i].innerHTML
+      var withoutTask = text.split("Task ")[1]
+      return removeSpacesAndReplaceDashes(withoutTask);
+    }
+  }
+}
+
+function hasNumber(myString) {
+  return /\d/.test(myString);
+}
+
+function removeSpacesAndReplaceDashes(string) {
+  var str = string.replace(/\s+/g, '-');
+  return str.replace(/:/g, '').toLowerCase();
 }
 
 function onWindowLoad() {
